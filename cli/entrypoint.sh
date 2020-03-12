7f2438dded0c8b07457d7820e240da957633cade
Kyle Harding <kmharding@pm.me>
2020-03-12 09:38:14 -0400

    use rsync to mirror default cli config to homeassistant config/.ssh

--- cli/entrypoint.sh
+++ cli/entrypoint.sh
@@ -1,24 +1,19 @@
 #!/bin/sh
 
-if [ ! -f /keys/ssh_host_rsa_key ] \
-|| [ ! -f /keys/ssh_host_dsa_key ] \
-|| [ ! -f /keys/ssh_host_ecdsa_key ] \
-|| [ ! -f /keys/ssh_host_ed25519_key ]
-then
-    /usr/bin/ssh-keygen -A
-    mv /etc/ssh/ssh_host_* /keys/
-fi
+/usr/bin/ssh-keygen -A
+mkdir -m 700 /config/.ssh 2>/dev/null || true
+rsync -av --ignore-existing /etc/ssh/ /config/.ssh/
 
 if curl -fsSL "${AUTHORIZED_KEYS_URL}" 1>/dev/null 2>&1
 then
-    curl -fsSL "${AUTHORIZED_KEYS_URL}" | tee /keys/authorized_keys
+    curl -fsSL "${AUTHORIZED_KEYS_URL}" > /config/.ssh/authorized_keys
 fi
 
-chown -R root:root /keys
-chmod -R 700 /keys
+chown -R root:root /config/.ssh
+chmod -R 700 /config/.ssh
 
-echo "export HASS_SERVER=${HASS_SERVER}" | tee -a /etc/profile
-echo "export HASS_TOKEN=${HASS_TOKEN}" | tee -a /etc/profile
-echo "cd /config" | tee -a /etc/profile
+echo "export HASS_SERVER=${HASS_SERVER}" >> /etc/profile
+echo "export HASS_TOKEN=${HASS_TOKEN}" >> /etc/profile
+echo "cd /config" >> /etc/profile
 
 exec "$@"
